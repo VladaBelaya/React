6de31bfa3acb0bd3f957aba29c07e9af6a14a59a
VladaBelaya <vlada_chvanova@mail.ru>
2025-01-14 14:08:55 +0300

    task_5

--- app.js
+++ app.js
@@ -1,65 +1,49 @@
-// Сложение
-var getSumNumbers = function (value1, value2, system) {
-    var result = value1 + value2;
-    console.log("\u0421\u043B\u043E\u0436\u0435\u043D\u0438\u0435: ".concat(value1, " + ").concat(value2, " = ").concat(result.toString(system), " (\u0432 ").concat(system, "-\u043E\u0439 \u0441\u0438\u0441\u0442\u0435\u043C\u0435 \u0441\u0447\u0438\u0441\u043B\u0435\u043D\u0438\u044F)"));
-    // Переводим в строку для вывода в нужной системе счисления
-    return result.toString(system);
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-var getDifferenceNumbers = function (value1, value2, system) {
-    var result = value1 - value2;
-    console.log("\u0420\u0430\u0437\u043D\u043E\u0441\u0442\u044C: ".concat(value1, " - ").concat(value2, " = ").concat(result.toString(system), " (\u0432 ").concat(system, "-\u043E\u0439 \u0441\u0438\u0441\u0442\u0435\u043C\u0435 \u0441\u0447\u0438\u0441\u043B\u0435\u043D\u0438\u044F)"));
-    // Переводим в строку для вывода в нужной системе счисления
-    return result.toString(system);
-};
-var getMultiplyNumbers = function (value1, value2, system) {
-    var result = value1 * value2;
-    console.log("\u0420\u0430\u0437\u043D\u043E\u0441\u0442\u044C: ".concat(value1, " * ").concat(value2, " = ").concat(result.toString(system), " (\u0432 ").concat(system, "-\u043E\u0439 \u0441\u0438\u0441\u0442\u0435\u043C\u0435 \u0441\u0447\u0438\u0441\u043B\u0435\u043D\u0438\u044F)"));
-    // Переводим в строку для вывода в нужной системе счисления
-    return result.toString(system);
-};
-var getDivisionNumbers = function (value1, value2, system) {
-    if (value2 === 0) {
-        console.log("\u041E\u0448\u0438\u0431\u043A\u0430: \u041D\u0430 0 \u0434\u0435\u043B\u0438\u0442\u044C \u043D\u0435\u043B\u044C\u0437\u044F");
-        return null;
+function loadComponent() {
+    return __awaiter(this, void 0, void 0, function* () {
+        var _a;
+        try {
+            const response = yield fetch('component.json');
+            if (!response.ok) {
+                console.log('Ошибка при запросе:', response);
+                return;
+            }
+            const component = yield response.json();
+            const element = createElement(component);
+            (_a = document.getElementById('app')) === null || _a === void 0 ? void 0 : _a.appendChild(element);
+        }
+        catch (error) {
+            console.error('Ошибка при загрузке компонента:', error);
+        }
+    });
+}
+function createElement(virtualNode) {
+    var _a;
+    const element = document.createElement(virtualNode.tag);
+    for (const [key, value] of Object.entries((_a = virtualNode.attrs) !== null && _a !== void 0 ? _a : {})) {
+        element.setAttribute(key, value);
     }
-    var result = value1 / value2;
-    console.log("\u0420\u0430\u0437\u043D\u043E\u0441\u0442\u044C: ".concat(value1, " / ").concat(value2, " = ").concat(result.toString(system), " (\u0432 ").concat(system, "-\u043E\u0439 \u0441\u0438\u0441\u0442\u0435\u043C\u0435 \u0441\u0447\u0438\u0441\u043B\u0435\u043D\u0438\u044F)"));
-    // Переводим в строку для вывода в нужной системе счисления
-    return result.toString(system);
-};
-// СЛОЖЕНИЕ
-// В 10-ной системе счисления
-var firstValueSum = 18;
-var secondValueSum = 32;
-getSumNumbers(firstValueSum, secondValueSum, 10);
-// В 2-ной системе счисления
-getSumNumbers(firstValueSum, secondValueSum, 2);
-// В 16-ной системе счисления
-getSumNumbers(firstValueSum, secondValueSum, 16);
-// РАЗНОСТЬ
-// В 10-ной системе счисления
-var firstValueDiff = 50;
-var secondValueDiff = 32;
-getDifferenceNumbers(firstValueDiff, secondValueDiff, 10);
-// В 2-ной системе счисления
-getDifferenceNumbers(firstValueDiff, secondValueDiff, 2);
-// В 16-ной системе счисления
-getDifferenceNumbers(firstValueDiff, secondValueDiff, 16);
-// УМНОЖЕНИЕ
-// В 10-ной системе счисления
-var firstValueMultiply = 50;
-var secondValueMultiply = 32;
-getMultiplyNumbers(firstValueMultiply, secondValueMultiply, 10);
-// В 2-ной системе счисления
-getMultiplyNumbers(firstValueMultiply, secondValueMultiply, 2);
-// В 16-ной системе счисления
-getMultiplyNumbers(firstValueMultiply, secondValueMultiply, 16);
-// ДЕЛЕНИЕ
-// В 10-ной системе счисления
-var firstValueDiv = 20;
-var secondValueDiv = 4;
-getDivisionNumbers(firstValueDiv, secondValueDiv, 10);
-// В 2-ной системе счисления
-getDivisionNumbers(firstValueDiv, secondValueDiv, 2);
-// В 16-ной системе счисления
-getDivisionNumbers(firstValueDiv, secondValueDiv, 16);
+    // Обрабатываем дочерние элементы
+    virtualNode.children.forEach(child => {
+        if (typeof child === 'string') {
+            // Если дочерний элемент - это текст, добавляем его как текстовый узел
+            element.appendChild(document.createTextNode(child));
+        }
+        else {
+            // Рекурсивно создаем дочерние элементы
+            element.appendChild(createElement(child));
+        }
+    });
+    return element;
+}
+// Вызываем функцию загрузки компонента
+loadComponent();
